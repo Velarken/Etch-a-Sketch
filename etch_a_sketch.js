da58fe8ce8a45efdafbc635d578bd04d084f3693
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-04-08 10:51:08 -0400

    added basic function

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,7 +1,3 @@
-
-const gridContainer = document.getElementById('#gridContainer');
-document.createElement('div');
-
 // Generate baseline grid, 16x16
     // function for generating divs defaults to 16x16
 
@@ -17,3 +13,31 @@ document.createElement('div');
 // Generate new grid based off of user input
     // Create number of columns based off user input
     // For each column, add same number of rows to each column
+const gridContainer = document.querySelector('#gridContainer');
+gridContainer.addEventListener('mouseover', changeColor);
+
+function generateGrid(num) {
+    // Constructors
+    // Generate Columns
+    gridContainer.replaceChildren();
+    for (let y=0; y < num; y++) {
+        let column = document.createElement('div');
+        column.className = 'column';
+        // Generate Rows
+        for (let x=0; x < num; x++) {
+            let row = document.createElement('row');
+            row.className = 'row';
+            row.id = x + ',' + y;
+            column.appendChild(row);
+        }
+        gridContainer.appendChild(column);
+    }
+}
+generateGrid(50);
+
+// Hovering over a div changes its color to black
+function changeColor(event) {
+    if (event.target.className == 'row') {
+        event.target.style.backgroundColor = "blue";
+    }
+}
