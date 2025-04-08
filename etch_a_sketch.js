b87a8cc7b81abab2a3f2b5b9b6fcbcc7edc4d583
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-04-08 08:42:15 -0400

    added pseudocode

---
+++ etch_a_sketch.js
@@ -0,0 +1,19 @@
+
+const gridContainer = document.getElementById('#gridContainer');
+document.createElement('div');
+
+// Generate baseline grid, 16x16
+    // function for generating divs defaults to 16x16
+
+// Get Number of divs to generate via user input via popup from button click
+    // Track mouse event for clicking button
+    // When pressed, fire browser popup window
+    // Limit to 100 as maximum, if > 100, error
+    //
+
+// Clear current grid
+    //Reset grid back to 16x16 default
+
+// Generate new grid based off of user input
+    // Create number of columns based off user input
+    // For each column, add same number of rows to each column
